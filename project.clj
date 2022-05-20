8e991472c1433f82ead90a6d07a15c03dc932f48
mcorbin <corbin.math@gmail.com>
2022-05-20 19:32:33 +0200

    Bump commentator project

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject commentator "0.15.0"
+(defproject commentator "0.17.0"
   :description "A Free commenting system"
   :url "https://github.com/mcorbin/commentator"
   :license {:name "EPL-2.0"
